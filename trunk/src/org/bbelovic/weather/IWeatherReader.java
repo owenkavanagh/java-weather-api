917a19c52266fa12af4f909233e7126bf33dd6e1
bbelovic <bbelovic@1d77ceb0-c99e-eafc-159f-7fac43e5eff2>
2011-03-22 22:36:16 +0000

    set and get methods moved from AbstractWeatherReader to IWeatherReader, added build.xml for compiling, running test and building jar file.

--- trunk/src/org/bbelovic/weather/IWeatherReader.java
+++ trunk/src/org/bbelovic/weather/IWeatherReader.java
@@ -3,4 +3,8 @@ package org.bbelovic.weather;
 public interface IWeatherReader 
 {
 	public void process();
+	public String getLocation();
+	public void setLocation(String location);
+	public WeatherModel getWeatherModel();
+	public void setWeatherModel(WeatherModel wm);
 }
