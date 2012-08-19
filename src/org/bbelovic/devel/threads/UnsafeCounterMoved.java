449b7586ef7c71df4b25fc91e60414c1abbb915a
bbelovic <bbelovic@1d77ceb0-c99e-eafc-159f-7fac43e5eff2>
2012-08-19 19:34:01 +0000

    Renamed from UnsafeCounter.java

---
+++ src/org/bbelovic/devel/threads/UnsafeCounterMoved.java
@@ -0,0 +1,20 @@
+package org.bbelovic.devel.threads;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class UnsafeCounter {
+
+    private final AtomicInteger counter = new AtomicInteger(0);
+    
+    public int getCounter() {
+        return counter.get();
+    }
+    
+    /**
+     * This method increments counter safely.
+     */
+    public void doIt() {
+        System.out.println(Thread.currentThread().getName() + ": counter "
+                + counter.incrementAndGet());
+    }
+}
