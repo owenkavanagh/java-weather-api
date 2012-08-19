7bb86608a72d0a6374289ef2988409c7d61c813b
bbelovic <bbelovic@1d77ceb0-c99e-eafc-159f-7fac43e5eff2>
2012-08-19 20:19:17 +0000

    Yet another fictional conflict.

--- trunk/src/org/bbelovic/devel/threads/UnsafeCounterMoved.java
+++ trunk/src/org/bbelovic/devel/threads/UnsafeCounterMoved.java
@@ -11,7 +11,7 @@ public class UnsafeCounterMoved {
     }
     
     /**
-     * This method increments counter safely.
+     * This method increments counter safely. Making a conflict here :)
      */
     public void doIt() {
         System.out.println(Thread.currentThread().getName() + ": counter "
