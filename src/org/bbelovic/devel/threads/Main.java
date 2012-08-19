30cb14cdd76d4dea2ad9d763bd1c02df2258761b
bbelovic <bbelovic@1d77ceb0-c99e-eafc-159f-7fac43e5eff2>
2012-08-19 19:28:49 +0000

    Tweaks..

--- src/org/bbelovic/devel/threads/Main.java
+++ src/org/bbelovic/devel/threads/Main.java
@@ -3,17 +3,37 @@ package org.bbelovic.devel.threads;
 public class Main {
 
     public static void main(String[] args) {
-        final UnsafeCounter unsafeCounter = new UnsafeCounter();
-        Thread t1 = new Thread(new ThreadWorker(unsafeCounter));
-        Thread t2 = new Thread(new ThreadWorker(unsafeCounter));
-        Thread t3 = new Thread(new ThreadWorker(unsafeCounter));
-        Thread t4 = new Thread(new ThreadWorker(unsafeCounter));
-        Thread t5 = new Thread(new ThreadWorker(unsafeCounter));
+//        final UnsafeCounter unsafeCounter = new UnsafeCounter();
+//        Thread t1 = new Thread(new ThreadWorker(unsafeCounter));
+//        Thread t2 = new Thread(new ThreadWorker(unsafeCounter));
+//        Thread t3 = new Thread(new ThreadWorker(unsafeCounter));
+//        Thread t4 = new Thread(new ThreadWorker(unsafeCounter));
+//        Thread t5 = new Thread(new ThreadWorker(unsafeCounter));
 
-        t1.start();
-        t2.start();
-        t3.start();
-        t4.start();
-        t5.start();
+//        t1.start();
+//        t2.start();
+//        t3.start();
+//        t4.start();
+//        t5.start();
+        
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i + "! = " + factorial(i));
+        }
+        
+    }
+    
+    public static long factorial(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("Factorial for negative input is not defined.");
+        }
+        if (n == 0 || n == 1) {
+            return 1L;
+        } else {
+            long pom = 1L;
+            for (int i = 2; i <= n; i++) {
+                pom *= i;
+            }
+            return pom;
+        }
     }
 }
