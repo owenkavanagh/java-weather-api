5f5ff05b78e639991d14916edd792e2170f90f4c
bbelovic <bbelovic@1d77ceb0-c99e-eafc-159f-7fac43e5eff2>
2012-08-25 13:59:15 +0000

    Added thread servlet example.

--- src/org/bbelovic/devel/threads/clients/Client.java
+++ src/org/bbelovic/devel/threads/clients/Client.java
@@ -9,12 +9,18 @@ import java.net.Socket;
 
 public class Client implements Runnable {
 
+    private static final Object lock = new Object();
+    private static final String URI_PATH = "/counter-example/synchronizedcounter";
+    private static int instanceCounter = 0;
     private final String url;
+    private final String name;
     private final int iterations;
+    
     
     public Client(String url, int iterations) {
         this.url = url;
-        this.iterations= iterations;
+        this.iterations = iterations;
+        this.name = "Client-"+ nextID();
     }
 
     @Override
@@ -24,28 +30,38 @@ public class Client implements Runnable {
         PrintWriter out = null;
 
         try {
-            socket = new Socket(url, 8080);
-            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            out = new PrintWriter(socket.getOutputStream(), true);
+            
             for (int i = 0; i < iterations; i++) {
-                out.print("GET /unsynchronizedcounter HTTP/1.0 \r\n");
-                out.print("\r\n");
-                out.flush();
-                System.out.println(in.readLine());
+                socket = new Socket(url, 8080);
+                synchronized (lock) {
+                    in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                    out = new PrintWriter(socket.getOutputStream(), true);
+                    out.print("GET "+ URI_PATH +" \r\n");
+                    out.print("\r\n");
+                    out.flush();
+                
+                    System.out.println(name + ": " +in.readLine());
+                }
+                close(out);
+                close(in);
             }
+            
         } catch (final IOException ioe) {
             System.out.println("Error in socket communication: " + ioe.getMessage());
-        } finally {
-            close(out);
-            close(in);
-        }
+        } 
     }
     
     private void close(Closeable c) {
         try {
-            c.close();
+            if (c != null) {
+                c.close();
+            }
         } catch (final IOException ioe) {
             System.out.println("Error when closing streams: "+ ioe.getMessage());
         }
+    }
+    
+    private static synchronized int nextID() {
+        return instanceCounter++;
     }
 }
